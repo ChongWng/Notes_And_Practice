4b47afb3dbca8afd45e91c3d829046da82a67c12
Chong Wang <wcssdy@gmail.com>
2017-11-25 22:04:55 +1300

    ng

--- Angular/ts-hello/main.ts
+++ Angular/ts-hello/main.ts
@@ -1,11 +1,14 @@
 
-/* function doSomething(){
+//---------------------
+function doSomething(){
     for(var i=0;i<5;i++){
         console.log(i);
     }
     console.log('Finanlly:'+i);
 }
-doSomething(); */
+doSomething(); 
+
+//-------------------------
 
 let a: number;
 let b: boolean;
@@ -17,5 +20,47 @@ let f: any[] = [true, 'Hello', 2];
 const ColorRed = 0;
 const ColorBlue = 1;
 
-enum Color {Red, Blue, Green};
+enum Color { Red = 0, Blue = 1, Green = 2};
+//better to set the value for each color explicitly
 let bgColor = Color.Blue;
+
+//------------------
+//Type assertion
+let message;
+message = 'Hello World';
+let endsWithD = (<string>message).endsWith('d');
+let alternativeWay = (message as string).endsWith('d');
+
+
+let log = function(message){
+    console.log(message);
+} //way in JS
+//arrow function or lambda expression in C# and Java
+let doLog = (message) => console.log(message);
+let doLog2 = message => console.log(message);
+let doLog3 = () => console.log();
+
+//----------------------------
+//inline annotation
+let drawPoint = (point: {x: number, y: number}) => {
+    //...
+}
+
+drawPoint({
+    x: 1,
+    y: 2
+})
+
+//interface, reusable
+interface Point{
+    x: number,
+    y: number
+}
+let alternativeDrawPoint = (point: Point) => {
+    //...
+}
+
+drawPoint({
+    x: 1,
+    y: 2
+})
